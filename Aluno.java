cb46efb4fe3f05902c6631bcbb74a5fda44bf650
Marcelo Bresani Victor De Oliveira <169096428+mbvo1@users.noreply.github.com>
2025-04-11 17:07:10 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,1 +1,16 @@
+public class Aluno {
+    String nome;
+    String ra;
+    double nota;
+
+    public Aluno(String nome, String ra, double nota) {
+        this.nome = nome;
+        this.ra = ra;
+        this.nota = nota;
+    }
+
+    public String toString() {
+        return "Nome: " + nome + ", RA: " + ra + ", Nota: " + nota;
+    }
+}
 
